aab13b85e1a54dc04f395e9c63e339c9c78dd869
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-03-21 15:12:08 -0400

    Added Cors configs

--- src/main/java/com/genspark/ToDoList/ToDoListApplication.java
+++ src/main/java/com/genspark/ToDoList/ToDoListApplication.java
@@ -1,7 +1,12 @@
 package com.genspark.ToDoList;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ToDoListApplication {
@@ -10,4 +15,17 @@ public class ToDoListApplication {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
